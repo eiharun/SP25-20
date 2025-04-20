64400a3fdb8bdeb392063ae466b37d94726c1ba6
Harun Adiyaman <haruna@vt.edu>
2025-04-20 00:32:25 -0400

    Cleaned up TUI
    Added a set recieve timeout command and added help documentation

--- RaspberryPi/UI/rfm95api/commands.py
+++ RaspberryPi/UI/rfm95api/commands.py
@@ -5,15 +5,14 @@ class Commands(Enum):
     #############
     IDLE=1      # ONLY USED BY THE BALLOON
     CUTDOWN=2   # Cutdown the balloon
-    CLOSE=3    # Close the vent???
+    CLOSE=3     # Close the vent
     #up to 11
     OPENs=64    # Open for x seconds, then close
-    OPENm=65    # Open for x minutes, then close
     #up to 193
     #commands with tbd data encoding schemes
     TEST=194    # Test command
     ##############
-    BUSY=255  # Flag sent from balloon to indicate that the vent is open???
+    BUSY=255  # Flag sent from balloon to indicate that the vent is open
     #up to 255
 
 # Structure of the commands Enum:
--- RaspberryPi/UI/tui/tui.py
+++ RaspberryPi/UI/tui/tui.py
@@ -22,6 +22,10 @@ class TUI:
             print("Starting TUI")
             tui_command = TUICommand(self.rfm95)
             tui_command.cmdloop()
+        except AssertionError as e:
+            print(f"AssertionError: {e}")
+            logger.error(f"AssertionError: {e}")
+            print("Please revise your input\nRFM95> ", end="")
         except KeyboardInterrupt:
             print()
             logger.debug("Exiting Ground Station TUI")
@@ -39,34 +43,30 @@ class TUICommand(cmd.Cmd):
         super().__init__()
         self.rfm95 = rfm95
         
-        self.intro = "Welcome to the RFM95 CLI. Type help or ? to list commands.\n"
-        self.intro += "Type 'exit' to exit the CLI.\n"
-        self.intro += "Type 'help <command>' for help on a specific command.\n"
+        self.intro = "Welcome to the RFM95 CLI.\n\n"
         self.intro += "Type 'clear' to clear the screen.\n"
+        self.intro += "Type 'help' or '?' a list of commands.\n"
+        self.intro += "Type 'help <command>' for help on a specific command.\n"
+        self.intro += "Type 'exit' to exit the CLI.\n"
         
         self.send_on_idle = True
         self._idle_status = False
         self.seq = 0
         self.cmd = Commands.DEFAULT.value
+        self.RFMtimeout = 5
         
     #----------------COMMANDS SET-------------------------#
     def do_open(self, arg):
-        """Open the vent for 'x' (sec/min)
-    Usage: OPEN x [s/m]
-    \t x - number of seconds
-    \t [s/m] - specify s or m for seconds or minutes
-    To open the vent for 5 minutes
-    \tOPEN 5 m 
-    To open the vent for 10 seconds
-    \tOPEN 10 s
-    """
-        assert len(arg.split(' ')) == 2
+        """Open the balloon vent for a given duration"""
+        assert len(arg.split(' ')) == 2, "Invalid number of arguments. Requires at 2. Usage: open x [s/m]"
         duration = int(arg.split(' ')[0])
+        assert duration > 0, "Duration must be greater than 0"
         unit = arg.split(' ')[1].lower()
-        assert duration < 2**(8*8) #Max len is 8
-        assert unit == 's' or unit == 'm'
-        self.cmd = Commands.OPENs.value if unit == 's' else Commands.OPENm.value
-        assert self.cmd != Commands.DEFAULT.value
+        assert unit == 's' or unit == 'm', "Invalid unit. Must be 's' or 'm'"
+        self.cmd = Commands.OPENs.value 
+        if unit == 'm':
+            duration = duration * 60 # Instead of having an OPENm command, we convert minutes to seconds as 4 bytes is enough to represent 50k days
+        assert duration < 2**(4*8), "Duration is too long. Max duration is 2^32-1 seconds [4 bytes]"
         num_bytes, payload = self.byte_w_len(duration)
         self.rfm95.send(payload, seq=self.seq, ack=0, CMD=self.cmd, length=num_bytes)
 
@@ -75,7 +75,7 @@ class TUICommand(cmd.Cmd):
         print(f"Sent Headers: {self.seq} {self.cmd} {num_bytes}")
 
         self.seq = (self.seq+1)%256
-        recv = self.rfm95.receive(timeout=5)
+        recv = self.rfm95.receive(timeout=self.RFMtimeout)
 
         if recv is None:
             print("No Ack recieved. Verify gps data before resending")
@@ -89,13 +89,12 @@ class TUICommand(cmd.Cmd):
 
     def do_cutdown(self, arg):
         """"Cutdown the balloon"""
-
         answer = input("Are you sure you want to cutdown the balloon? y/n:")
         if answer.lower() == 'y':
             print("Sending cutdown command")
             self.rfm95.send(b'', seq=self.seq, ack=0, CMD=Commands.CUTDOWN.value, length=0)
             self.seq = (self.seq+1)%256
-            recv = self.rfm95.receive(timeout=5)
+            recv = self.rfm95.receive(timeout=self.RFMtimeout)
 
             if recv is None:
                 print("No Ack recieved. Verify gps data before resending")
@@ -113,11 +112,10 @@ class TUICommand(cmd.Cmd):
     def do_idle(self, arg):
         """Sends an IDLE command to the balloon 
         Usage: IDLE"""
-
         print("Sending idle command")
         self.rfm95.send(b'', seq=self.seq, ack=0, CMD=Commands.IDLE.value, length=0)
         self.seq = (self.seq+1)%256
-        recv = self.rfm95.receive(timeout=5)
+        recv = self.rfm95.receive(timeout=self.RFMtimeout)
         if recv is None:
             print("No Ack recieved. Verify gps data before resending")
         else:
@@ -129,28 +127,75 @@ class TUICommand(cmd.Cmd):
             print(f"\tSNR: {self.rfm95.last_snr}")
     
     def do_close(self, arg):
-        """"Cutdown the balloon"""
-        
+        """"Closes the balloon vent""" 
         answer = input("Are you sure you want to close the balloon? y/n:")
         if answer.lower() == 'y':
             print("Sending close command")
             self.rfm95.send(b'', seq=self.seq, ack=0, CMD=Commands.CLOSE.value, length=0)
             self.seq = (self.seq+1)%256
-            recv = self.rfm95.receive(timeout=5)
+            recv = self.rfm95.receive(timeout=self.RFMtimeout)
 
             if recv is None:
                 print("No Ack recieved. Verify gps data before resending")
             else:
                 seq,ack,cmd,length,data = self.rfm95.extractHeaders(recv)
-                if cmd == 255:
-                    print("Motor is open. Closing")
+                print("Motor is open. Closing")
                 print(f"Recieved Headers: {seq} {ack} {cmd} {length} {data}")
                 print(f"\tSignal Strength: {self.rfm95.last_rssi}")
                 print(f"\tSNR: {self.rfm95.last_snr}")
         else:
             print("Ok, returning")
             return
+    #----------------MAN PAGES----------------------------#    
+        
+    def help_open(self):
+        print("Open the vent for 'x' (sec/min)")
+        print("Usage: OPEN x [s/m]")
+        print("\t x - number of seconds")
+        print("\t [s/m] - specify s or m for seconds or minutes")
+        print("To open the vent for 5 minutes")
+        print("\tOPEN 5 m")
+        print("To open the vent for 10 seconds")
+        print("\tOPEN 10 s")
+    
+    def help_cutdown(self):
+        print("Cutdown the balloon")
+        print("Usage: cutdown")
+        print("This will send a cutdown command to the balloon")
+        print("This will not close the vent. You must send a close command to close the vent")
+    
+    def help_idle(self):
+        print("Send an IDLE command to the balloon")
+        print("Usage: idle")
+        print("This will send an IDLE command to the balloon")
+        print("This will do nothing on the balloon. Expect an acknowledgement if the balloon is in range")
+    
+    def help_close(self):
+        print("Close the vent")
+        print("Usage: close")
+        print("This will send a close command to the balloon")
+        print("This will close the vent if it is open. Expect an acknowledgement if the balloon is in range")
+    
+    def help_set_timeout(self):
+        print("Set the receive timeout for the RFM95")
+        print("Usage: set_timeout x")
+        print("\t x - number of seconds")
+        print("This will set the timeout for the RFM95 to 'x' seconds")
+        print("This will not affect the balloon. It is only for the ground station")
+        print("The default timeout is 5 seconds")
+        print(f"Current timeout is {self.RFMtimeout} seconds")
+        print("To set the timeout to 10 seconds")
+        print("\tset_timeout 10")
+        
     #----------------MISC---------------------------------#    
+        
+    def do_set_timeout(self, arg):
+        assert len(arg.split(' ')) == 1, "Invalid number of arguments. Requires 1. Usage: set_timeout x"
+        timeout = int(arg.split(' ')[0])
+        assert timeout > 0, "Timeout must be greater than 0"
+        assert timeout < 30, "Timeout must be less than 30 seconds (if you're not getting a response after a 30 second timeout then something is wrong)"
+        self.RFMtimeout = timeout
+        print(f"Timeout set to {self.RFMtimeout} seconds")
         
     def preloop(self):
         """Initialization before the command loop starts."""
@@ -177,6 +222,10 @@ class TUICommand(cmd.Cmd):
         print("\033[H\033[J", end="")
         print(self.intro)
         return False
+    
+    def get_names(self):
+        print(n for n in dir(self.__class__))
+        return [n for n in dir(self.__class__) if n not in self.__hiden_methods]
     
     #----------------HELPERS------------------------------#    
     
