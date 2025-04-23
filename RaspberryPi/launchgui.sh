bc2e683073d2f3b127e674a85fc93abcf4b4bb64
Harun raspi <haruna@vt.edu>
2025-04-22 22:22:54 -0400

    s

--- RaspberryPi/launchgui.sh
+++ RaspberryPi/launchgui.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/home/HAB/SP25-20/RaspberryPi
-python3 -m UI -D -f "/home/HAB/SP25-20/RaspberryPi/UI/logs/UI.log"
+python3 -m UI -D -f "/home/HAB/SP25-20/RaspberryPi/UI/logs/UI.log" 
 
-sleep 10
