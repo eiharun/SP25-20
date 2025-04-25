d0c0a845f5544b95964c8b0d56646a558f3d14b4
Harun Adiyaman <haruna@vt.edu>
2025-04-25 17:59:18 -0400

    removed .vscode

--- RaspberryPi/UI/rfm95api/commands.py
+++ RaspberryPi/UI/rfm95api/commands.py
@@ -10,7 +10,7 @@ class Commands(Enum):
     OPEN=64    # Open for x seconds, then close
     #up to 193
     #commands with tbd data encoding schemes
-    TEST=194    # Test command
+    # ...
     ##############
     BUSY=255  # Flag sent from balloon to indicate that the vent is open
     #up to 255
@@ -19,6 +19,6 @@ class Commands(Enum):
 #0: Reserved DEFAULT
 #1-63: Reserved for commands with no data payload (len=0)
 #64-191: Reserved for commands with binary number data payloads (with MAX len=8) This is plenty as it can represent 5849424173.55072 centuries duration
-#192-255: Reserved for commands with TBD encoded data payloads
+#192-254: Reserved for commands with TBD encoded data payloads
 # Subject to change as more commands are added
-
+#255: Reserved for BUSY flag - may optionally have payload to indicate which resource is busy if needed
